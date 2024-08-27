67fce31b72e66d43ba8c29c8c8b4ee9ed13d4a0d
Srikargoud6 <srikargoud611@gmail.com>
2024-08-27 23:01:39 +0530

    added the explore menu section

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Menu.css";
+import { menu_list } from "../../assets/frontend_assets/assets";
+import "bootstrap/dist/css/bootstrap.css";
+
+const Menu = ({ category, setCategory }) => {
+  return (
+    <div className="menu container">
+      <h1>Explore our menu</h1>
+      <div className="d-flex justify-content-between row">
+        {menu_list.map((item) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "all" : item.menu_name
+                )
+              }
+              className="menu-item d-flex flex-column align-items-center col-xs-6 col-sm-3 col-md-4 col-lg-1"
+            >
+              <img
+                src={item.menu_image}
+                alt="menu"
+                className={category === item.menu_name ? "active" : ""}
+              />
+              <h5>{item.menu_name}</h5>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
+import Menu from "../../components/Menu/Menu";
+import { useState } from "react";
 
 const Home = () => {
+  const [category, setCategory] = useState("all");
+
   return (
     <div className="d-flex flex-column align-items-center">
       <Header />
+      <Menu category={category} setCategory={setCategory} />
     </div>
   );
 };
