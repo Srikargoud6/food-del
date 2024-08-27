35ca485e8c0b271b454b03068c8d39d515c31881
Srikargoud6 <srikargoud611@gmail.com>
2024-08-27 08:44:22 +0530

    added header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./Header.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Header = () => {
   return (
     <div className="header">
-      <h2>Oder your favourite food here</h2>
-      <p>
-        Choose from diverse menu featuring a delectable array of dishes crafted
-        with the finest ingredients and culinary expertise. Our mission is to
-        satisfy your cravings and elevate your dining experience , one delicious
-        meal at a time.
-      </p>
+      <div className="header-content">
+        <h2>Oder your favourite food here</h2>
+        <p>
+          Choose from diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients and culinary expertise. Our
+          mission is to satisfy your cravings and elevate your dining experience
+          , one delicious meal at a time.
+        </p>
+        <button class="btn btn-light">View menu</button>
+      </div>
     </div>
   );
 };
