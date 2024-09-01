0a5746bd727e246f1bd6b6ca46eccdc20fe0a5d8
Srikargoud6 <srikargoud611@gmail.com>
2024-09-01 17:05:03 +0530

    added a feature to select the order count

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
+import { assets } from "../../assets/frontend_assets/assets";
 
 const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setItemCount] = useState(0);
+
   return (
     <div class="card food-card" style={{ width: "18rem" }}>
       <img class="card-img-top food-img" src={image} alt="Card image cap" />
+
       <div class="card-body">
-        <h5 class="card-title">{name}</h5>
+        <section className="d-flex justify-content-between align-items-center">
+          <div>
+            <h5 class="card-title">{name}</h5>
+          </div>
+          <div>
+            {!itemCount ? (
+              <img
+                src={assets.add_icon_white}
+                alt="add"
+                onClick={() => setItemCount((prev) => prev + 1)}
+                className="add-icon"
+              />
+            ) : (
+              <div className="d-felx">
+                <img
+                  src={assets.remove_icon_red}
+                  alt="remove"
+                  onClick={() => setItemCount((prev) => prev - 1)}
+                  className="add-icon mar1"
+                />
+                {itemCount}
+                <img
+                  src={assets.add_icon_green}
+                  alt="add"
+                  onClick={() => setItemCount((prev) => prev + 1)}
+                  className="add-icon mar2"
+                />
+              </div>
+            )}
+          </div>
+        </section>
         <p class="card-text">{description}</p>
         <h5 className="price">{`$${price}`}</h5>
       </div>
