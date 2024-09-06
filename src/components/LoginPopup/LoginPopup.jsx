5ad9b07efd3ced5699303117a7b23bd27021eed8
Srikargoud6 <srikargoud611@gmail.com>
2024-09-06 13:12:06 +0530

    added styles for login/signup window

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -5,17 +5,18 @@ import { assets } from "../../assets/frontend_assets/assets";
 const LoginPopup = ({ setShowLogin }) => {
   const [currentState, setCurrentState] = useState("Sign Up");
   return (
-    <div>
-      <form className="login-popup">
-        <div>
+    <div className="login-popup">
+      <form className="login-popup-form">
+        <div className="d-flex justify-content-between align-items-start text-dark">
           <h2>{currentState}</h2>
           <img
             src={assets.cross_icon}
             alt=""
             onClick={() => setShowLogin(false)}
+            className="clickable cross-icon"
           />
         </div>
-        <div>
+        <div className="inputs">
           {currentState === "Login" ? (
             <></>
           ) : (
@@ -26,26 +27,42 @@ const LoginPopup = ({ setShowLogin }) => {
             type="password"
             name=""
             id=""
-            placeholder="enter password"
+            placeholder="Enter password"
             required
           />
         </div>
         <button>
           {currentState === "Sign Up" ? "Create account" : "Login"}
         </button>
-        <div className="login-condition">
-          <input type="checkbox" name="" id="" required />
+        <div className=" d-flex align-items-start gap-2">
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            required
+            className="login-condition"
+          />
           <p>By continuing I agree to the terms of use and privacy policy</p>
         </div>
         {currentState === "Login" ? (
           <p>
             Create a new account?{" "}
-            <span onClick={() => setCurrentState("Sign Up")}>click here</span>
+            <span
+              onClick={() => setCurrentState("Sign Up")}
+              className="clickable"
+            >
+              click here
+            </span>
           </p>
         ) : (
           <p>
             Already have an account?{" "}
-            <span onClick={() => setCurrentState("Login")}>Login here</span>
+            <span
+              onClick={() => setCurrentState("Login")}
+              className="clickable"
+            >
+              Login here
+            </span>
           </p>
         )}
       </form>
